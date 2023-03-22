2690302e7ec690595732851e366cd5a53c37b2a8
davidecaputo <daca9621@gmail.com>
2023-03-22 21:01:37 +0100

    Create const for random numbers

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
     Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri che ha visto precedentemente.
     Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+// prendo il bottone
+const btn = document.querySelector('button');
+
+// genero i 5 numeri randomici
+const numberPc = [];
+while(numberPc.length < 5){
+    const numberRandom = getRndInteger(1, 1000);
+    numberPc.push(numberRandom);
+}
+console.log(numberPc);
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
